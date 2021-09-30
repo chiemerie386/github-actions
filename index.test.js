0ab27b0ab984067fbdb4346a30acc77d32c9b0c7
Chiemerie Anaebo <31117935+chiemerie386@users.noreply.github.com>
2021-09-30 06:08:03 +0100

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -5,5 +5,5 @@ test('mooncake knows what to say', () => {
 });
 
 test('mooncake is not sure what to say', () => {
-  expect(mooncakeSays()).toBe('(O.o) chookity?');
+  expect(mooncakeSays()).toBe('(o.o) chookity?');
 });
